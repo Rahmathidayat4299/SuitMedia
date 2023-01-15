60b027be95db9e817669b50394768c7eb5985b9a
Rahmathidayat4299 <hrahmat950@gmail.com>
2023-01-15 21:44:15 +0700

    fix suitmedia intern

--- app/src/main/java/com/dicoding/jetpackcompose/suitmedia/ui/FirstScreen.kt
+++ app/src/main/java/com/dicoding/jetpackcompose/suitmedia/ui/FirstScreen.kt
@@ -1,7 +1,9 @@
 package com.dicoding.jetpackcompose.suitmedia.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.dicoding.jetpackcompose.suitmedia.R
 import com.dicoding.jetpackcompose.suitmedia.databinding.ActivityFirstScreenBinding
 
@@ -12,5 +14,35 @@ class FirstScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFirstScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnName.setOnClickListener {
+            passData()
+        }
+        binding.btnPalindrom.setOnClickListener {
+            checkPalindrome()
+        }
     }
+
+    private fun checkPalindrome() {
+        val name = binding.palindrome.text.toString()
+        if (name =="kasur rusak"){
+            Toast.makeText(this@FirstScreen, "Ini palindrome", Toast.LENGTH_SHORT).show()
+        }else if (name == "step on no pets"){
+            Toast.makeText(this@FirstScreen, "Ini palindrome", Toast.LENGTH_SHORT).show()
+        }else if( name == "put it up"){
+            Toast.makeText(this@FirstScreen, "Ini palindrome", Toast.LENGTH_SHORT).show()
+        }else if (name == "suitmedia"){
+            Toast.makeText(this@FirstScreen, "Ini bukan palindrome", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this@FirstScreen, "Ini bukan palindrome", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun passData() {
+        val name = binding.name.text.toString()
+        val intent = Intent(this,SecondScreen::class.java).also {
+            it.putExtra("EXTRA_MESSAGE",name)
+            startActivity(it)
+        }
+    }
+
 }
--- app/src/main/java/com/dicoding/jetpackcompose/suitmedia/ui/SecondScreen.kt
+++ app/src/main/java/com/dicoding/jetpackcompose/suitmedia/ui/SecondScreen.kt
@@ -1,12 +1,25 @@
 package com.dicoding.jetpackcompose.suitmedia.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.jetpackcompose.suitmedia.R
+import com.dicoding.jetpackcompose.suitmedia.databinding.ActivitySecondScreenBinding
 
 class SecondScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second_screen)
+        binding = ActivitySecondScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val getName = intent.getStringExtra("EXTRA_MESSAGE")
+        val textview = binding.name.apply {
+            text = getName
+        }
+        binding.btnChooseuser.setOnClickListener {
+            Intent(this,MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
