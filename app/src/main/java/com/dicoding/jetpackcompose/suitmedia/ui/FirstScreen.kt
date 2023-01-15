072ea73fe3374f942bde6fdbaad86ba546bddc97
Rahmathidayat4299 <hrahmat950@gmail.com>
2023-01-15 21:09:10 +0700

    add second screen

--- app/src/main/java/com/dicoding/jetpackcompose/suitmedia/ui/FirstScreen.kt
+++ app/src/main/java/com/dicoding/jetpackcompose/suitmedia/ui/FirstScreen.kt
@@ -3,10 +3,14 @@ package com.dicoding.jetpackcompose.suitmedia.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.jetpackcompose.suitmedia.R
+import com.dicoding.jetpackcompose.suitmedia.databinding.ActivityFirstScreenBinding
+
 
 class FirstScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityFirstScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_screen)
+        binding = ActivityFirstScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/dicoding/jetpackcompose/suitmedia/ui/SecondScreen.kt
@@ -0,0 +1,12 @@
+package com.dicoding.jetpackcompose.suitmedia.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.jetpackcompose.suitmedia.R
+
+class SecondScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_screen)
+    }
+}
